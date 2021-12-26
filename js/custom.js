33a4969c33830752fcd760c319c45b23aba06159
imran khan <khanimran03466@gmail.com>
2021-12-26 13:02:08 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+const button = document.getElementById("form-button");
+const appDataBody = document.querySelector(".app-data-body > ul");
+const removeButton = document.getElementsByClassName("remove");
+
+button.onclick = function() {
+    var title = document.querySelector("#title").value;
+    var description = document.querySelector("#des").value;
+
+    if (title === "" || description === "") {
+        alert("Please fill all fileds");
+    } else {
+        const li = `<li>
+                <div class="data-body-card">
+                    <ul>
+                        <li>${title}</li>
+                        <li>${description} </li>
+                        <li> <a href="#" class="remove"> X </a> </li>
+                    </ul>
+                </div>
+            </li>`;
+
+        appDataBody.insertAdjacentHTML("afterbegin", li);
+
+        for (i = 0; i < removeButton.length; i++) {
+            removeButton[i].addEventListener("click", function(e) {
+                e.currentTarget.parentNode.parentNode.parentNode.remove();
+            })
+        }
+
+        document.querySelector("#title").value = "";
+        document.querySelector("#des").value = "";
+
+
+    }
+
+
+
+};
